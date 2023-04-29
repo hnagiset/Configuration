f29d90c422815480ee4c0cdfd203f7b3485cd475
user <user@localhost>
2023-04-29 09:36:41 +0530

    change ledger alignment column

--- .emacs.d/init/packages/ledger.el
+++ .emacs.d/init/packages/ledger.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
-(setq-default ledger-post-amount-alignment-column 58)
+(setq-default ledger-post-amount-alignment-column 80)
 (setq-default ledger-post-account-alignment-column 2)
 (setq-default ledger-highlight-xact-under-point nil)
 
