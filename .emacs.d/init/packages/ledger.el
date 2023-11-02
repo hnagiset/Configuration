75ba553a8742e67a5af343d1b62f4048ee341da9
user <user@localhost>
2023-11-02 17:01:59 +0530

    set prefs for firefox, disable autofill in ledger mode

--- .emacs.d/init/packages/ledger.el
+++ .emacs.d/init/packages/ledger.el
@@ -4,4 +4,6 @@
 (setq-default ledger-post-account-alignment-column 2)
 (setq-default ledger-highlight-xact-under-point nil)
 
+(add-hook 'ledger-mode-hook (lambda () (auto-fill-mode -1)))
+
 (/init/util/require-package 'ledger-mode)
--- Bash/hn-firefox.sh
+++ Bash/hn-firefox.sh
@@ -2,6 +2,15 @@
 
 PROFILE_DIR=$(mktemp -p /tmp -d firefox-profile.XXXXXX.d)
 
+echo 'user_pref("dom.event.clipboardevents.enabled", false);' >> "$PROFILE_DIR/user.js"
+echo 'user_pref("dom.event.contextmenu.enabled", false);' >> "$PROFILE_DIR/user.js"
+echo 'user_pref("browser.urlbar.placeholderName", "DuckDuckGo!");' >> "$PROFILE_DIR/user.js"
+echo 'user_pref("browser.newtabpage.enabled", false);' >> "$PROFILE_DIR/user.js"
+echo 'user_pref("datareporting.policy.firstRunURL", "");' >> "$PROFILE_DIR/user.js"
+echo 'user_pref("startup.homepage_welcome_url", "bing.com");' >> "$PROFILE_DIR/user.js"
+
+cp ~/.mozilla/firefox/*.default*/search.json.mozlz4 "$PROFILE_DIR"
+
 firefox -profile "$PROFILE_DIR" -no-remote -new-instance #-private-window
 
 rm -rf "$PROFILE_DIR"
