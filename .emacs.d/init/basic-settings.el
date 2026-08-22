0903e747fa67395c7409b2edcccb3b1cabcf6d79
user <user@localhost>
2026-08-22 18:23:28 +0530

    Save desktop file regularly

--- .emacs.d/init/basic-settings.el
+++ .emacs.d/init/basic-settings.el
@@ -58,4 +58,6 @@
 ;; Set highlight/region color to something more visible.
 (set-face-attribute 'region nil :background "#79fcaf")
 
+(run-with-idle-timer 300 t 'desktop-save-in-desktop-dir)
+
 (/init/util/personal-theme)
